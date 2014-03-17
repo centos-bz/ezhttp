b6b2f7cb461ee15f840f37ed9c25fb1286dec0e4
centos-bz <admin@centos.bz>
2014-03-17 23:53:18 +0800

    feature:增加设置iptables工具

--- start.sh
+++ start.sh
@@ -8,5 +8,21 @@ export PATH
 #   website: http://www.centos.bz/ezhttp/
 #===============================================================================
 
-echo "sorry,the install.sh script is no longer in use."
-echo "please excute start.sh script instead."
+cur_dir=`pwd`
+
+#初始化
+if [ -f $cur_dir/init ];then
+	. $cur_dir/init
+else
+	echo "init file not found.shell script can't be executed."
+	exit 1
+fi
+#载入常用函数
+if [ -f $cur_dir/func ];then
+	. $cur_dir/func
+else
+	echo "func file not found.shell script can't be executed."
+	exit 1
+fi	
+rm -f /root/ezhttp_errors.log
+deploy_linux 2>&1 | tee -a /root/ezhttp_errors.log
